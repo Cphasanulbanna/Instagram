93998375a7422e5ca83da20407d338c22de50904
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-06-03 06:59:15 +0530

    modal wrapper style completed

---
+++ client/src/components/reusable-components/ModalWrapper.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ModalWrapper = () => {
+    return (
+        <section className="w-[100%] h-[100%] flex justify-center items-center bg-[rgba(0,0,0,0.1)] fixed inset-0 z-50"></section>
+    );
+};
+
+export default ModalWrapper;
